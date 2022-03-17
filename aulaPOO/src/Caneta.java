489d09198a4f327b6496197bab9606529860673a
Kauan T. Eiras <43351064+kauaneiras@users.noreply.github.com>
2022-03-16 21:18:33 -0300

    Add files via upload

---
+++ aulaPOO/src/Caneta.java
@@ -0,0 +1,48 @@
+public class Caneta { //criando a classe caneta
+    //caracteristicas que uma caneta pode se classificar
+    private String modelo;
+    private float ponta;
+    private boolean tampada;
+    private String cor;
+
+    public Caneta(String modelo, String cor, float ponta) {
+        this.modelo = modelo;
+        this.ponta = ponta;
+        this.cor = cor;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public float getPonta() {
+        return ponta;
+    }
+
+    public void setPonta(float ponta) {
+        this.ponta = ponta;
+    }
+
+    public boolean isTampada() {
+        return tampada;
+    }
+
+    public void setTampada(boolean tampada) {
+        this.tampada = tampada;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+}
+
+
---
+++ aulaPOO/src/aula.java
@@ -0,0 +1,7 @@
+public class aula {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("NAT", "Roxo", 0.5f);
+        Caneta c2 = new Caneta ("GAB", "Amarelo", 1.0f);
+        Caneta c3 = new Caneta("KAU", "Verde", 1.5f);
+    }
+}
